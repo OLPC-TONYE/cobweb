bfd89abd82c6effc7b434279bf9871f8a7bba9ab
OLPC-TONYE <charlestonyeok@gmail.com>
2021-08-06 07:19:00 +0100

    Before adding the PPE feature

---
+++ src/main/java/org/cobweb/cobweb2/core/Drop.java
@@ -0,0 +1,16 @@
+package org.cobweb.cobweb2.core;
+
+
+/**
+ * Contains methods
+ *
+ */
+public interface Drop extends Updatable {
+	public boolean canStep(Agent agent);
+
+	public void onStep(Agent agent);
+
+	public void prepareRemove();
+
+	public int getProducerType();
+}
